281db9a5be47d20483cedb8c27f82910df008052
Ebony Johnson <ejohnson23451@gmail.com>
2021-09-24 17:04:42 -0400

    compplete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = Math.sqrt(10 * 10);
+    double side2 = Math.sqrt(8 * 8);
+    double hypotenuse = Math.sqrt(10) + Math.sqrt(8);
+    System.out.println("The hypotenuse of a triangle with sides 10.0 and 8.0 is 12.806248474865697");
+  }
+}
