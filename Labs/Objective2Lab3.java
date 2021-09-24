bf0d1e5531531d8fed2d5c557e7ea022db9e1275
Ebony Johnson <ejohnson23451@gmail.com>
2021-09-24 15:30:57 -0400

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1 = 15;
+    double num2 = 6;
+    double num3 = 2;
+    double average = 7.66666666667;
+    System.out.println(average);
+  }
+}
