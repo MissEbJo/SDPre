57c91e4110b45ac39e5680b30a6958f4933acc8b
Ebony Johnson <ejohnson23451@gmail.com>
2021-09-24 14:41:36 -0400

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Ebony Johnson ";
+    int age = 30;
+    String state = "South Carolina";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
