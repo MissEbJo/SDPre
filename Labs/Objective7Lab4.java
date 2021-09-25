4ca1af39996ba06eb674dacab85254a4b875d5ae
Ebony Johnson <ejohnson23451@gmail.com>
2021-09-25 15:14:27 -0400

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 0 , sum = 210;
+
+    while(count <=20) {
+      System.out.println(count + sum);
+
+      count = sum;
+    }
+  }
+}
