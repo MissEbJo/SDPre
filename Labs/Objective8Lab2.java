79c95a084c5954801fe418c9da37281a6e4b5a84
Ebony Johnson <ejohnson23451@gmail.com>
2021-09-26 16:17:28 -0400

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum =0;
+    int i = 20;
+
+    for(int control = 1; control <= i; control++){
+      sum = control + sum;
+    }
+    System.out.println(sum);
+  }
+}
